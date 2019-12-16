bab7d47960b06582f635b880e62dd59a32e50998
EugeneMonnier <eugene.monnier@gmail.com>
2019-12-16 15:40:34 -0800

    load random images to page through genRandomNum function

--- js/app.js
+++ js/app.js
@@ -4,9 +4,12 @@ var allProducts = [];
 var optionOne = 0;
 var optionTwo = 1;
 var optionThree = 2;
+var totalClicks = 0;
+var randoOne, randoTwo, randoThree;
+var productImage = document.getElementsByTagName('img');
 
 // Product constructor
-function getProducts(name, imageUrl) {
+function GetProducts(name, imageUrl) {
   this.name = name;
   this.imageUrl = imageUrl;
   this.score = 0;
@@ -14,23 +17,48 @@ function getProducts(name, imageUrl) {
 }
 
 // build allProducts array
-new getProducts('R2D2 Luggage', 'img/bag.jpg');
-new getProducts('Banana Slicer', 'img/banana.jpg');
-new getProducts('Toilet Stant', 'img/bathroom.jpg');
-new getProducts('Open-Toed Rain Boots', 'img/boots.jpg');
-new getProducts('Breakfast Station', 'img/breakfast.jpg');
-new getProducts('Meatball Bubble Gum', 'img/bubblegum.jpg');
-new getProducts('Concave Ass Chair', 'img/chair.jpg');
-new getProducts('Cthulu Figurine', 'img/cthulu.jpg');
-new getProducts('Dog Muzzle - Duck', 'img/dog-duck');
-new getProducts('Can o\' Dragon Meat', 'img/dragon.jpg');
-new getProducts('Pen Cap Utensils', 'img/pen.jpg');
-new getProducts('Pet Sweep Dog Shoes', 'img/pet-sweep.jpg');
-new getProducts('Pizza Scissors', 'img/scissors.jpg');
-new getProducts('Man-Eating Shark Sleeping Bag', 'img/shark.jpg');
-new getProducts('Sweeping Baby Onsie', 'img/sweep.png');
-new getProducts('Gutted Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
-new getProducts('Can o\' Unicorn Meat', 'img/unicorn.jpg');
-new getProducts('Tentacle USB Drive 128MB', 'img/usb.jpg');
-new getProducts('Self-Watering Water Can', 'img/water-can.jpg');
-new getProducts('Smell This Wine Glass', 'img/wine-glass.img');
+new GetProducts('R2D2 Luggage', 'img/bag.jpg');
+new GetProducts('Banana Slicer', 'img/banana.jpg');
+new GetProducts('Toilet Stant', 'img/bathroom.jpg');
+new GetProducts('Open-Toed Rain Boots', 'img/boots.jpg');
+new GetProducts('Breakfast Station', 'img/breakfast.jpg');
+new GetProducts('Meatball Bubble Gum', 'img/bubblegum.jpg');
+new GetProducts('Concave Ass Chair', 'img/chair.jpg');
+new GetProducts('Cthulu Figurine', 'img/cthulhu.jpg');
+new GetProducts('Dog Muzzle - Duck', 'img/dog-duck.jpg');
+new GetProducts('Can o\' Dragon Meat', 'img/dragon.jpg');
+new GetProducts('Pen Cap Utensils', 'img/pen.jpg');
+new GetProducts('Pet Sweep Dog Shoes', 'img/pet-sweep.jpg');
+new GetProducts('Pizza Scissors', 'img/scissors.jpg');
+new GetProducts('Man-Eating Shark Sleeping Bag', 'img/shark.jpg');
+new GetProducts('Sweeping Baby Onsie', 'img/sweep.png');
+new GetProducts('Gutted Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
+new GetProducts('Can o\' Unicorn Meat', 'img/unicorn.jpg');
+new GetProducts('Tentacle USB Drive 128MB', 'img/usb.gif');
+new GetProducts('Self-Watering Water Can', 'img/water-can.jpg');
+new GetProducts('Smell This Wine Glass', 'img/wine-glass.jpg');
+// debugger;
+// Generate random number be 0-19
+function genRandomNum() {
+  randoOne = Math.floor(Math.random() * 20);
+  while (randoOne === prevRandoOne || randoOne === prevRandoTwo || randoOne === prevRandoThree) {
+    randoOne = Math.floor(Math.random() * 20);
+  }
+  var prevRandoOne = randoOne;
+  randoTwo = Math.floor(Math.random() * 20);
+  while (randoTwo === prevRandoOne || randoTwo === prevRandoTwo || randoTwo === prevRandoThree || randoTwo === randoOne) {
+    randoTwo = Math.floor(Math.random() * 20);
+  }
+  var prevRandoTwo = randoTwo;
+  randoThree = Math.floor(Math.random() * 20);
+  while (randoThree === prevRandoOne || randoThree === prevRandoTwo || randoThree === prevRandoThree || randoThree === randoOne || randoThree === randoTwo){
+    randoThree = Math.floor(Math.random() * 20);
+  }
+  var prevRandoThree = randoThree;
+}
+
+genRandomNum();
+// Set product images 1-3
+productImage[1].src = allProducts[randoOne].imageUrl;
+productImage[2].src = allProducts[randoTwo].imageUrl;
+productImage[3].src = allProducts[randoThree].imageUrl;
